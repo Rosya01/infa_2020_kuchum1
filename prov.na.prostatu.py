87e23635383ec8d8ce29841084874d7f41dd19b2
Rosya01 <72784408+Rosya01@users.noreply.github.com>
2020-12-07 20:32:46 +0300

    Update prov.na.prostatu.py

--- prov.na.prostatu.py
+++ prov.na.prostatu.py
@@ -3,7 +3,7 @@ def IsPrime(n):
         return False
     if (n % 2 == 0 or n % 3 == 0):
         return True
-    i = 5
+    i = 2
     while(i * i <= n):
         if (n % i == 0 or n % (i + 2) == 0):
             return True
