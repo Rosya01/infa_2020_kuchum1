df0eb7495bc27cbd2262429ef550059639697a74
Rosya01 <72784408+Rosya01@users.noreply.github.com>
2020-12-07 20:28:48 +0300

    Add files via upload

---
+++ prov.na.prostatu.py
@@ -0,0 +1,16 @@
+def IsPrime(n):
+    if (n <= 1 or n <= 3):
+        return False
+    if (n % 2 == 0 or n % 3 == 0):
+        return True
+    i = 5
+    while(i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return True
+        i += 6
+    return False
+n = int(input())
+if IsPrime(n):
+    print("NO")
+else:
+    print("YES")
