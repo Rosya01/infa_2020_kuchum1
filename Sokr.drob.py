0e684847ebdb25650fe0e9d9e795cc3ff6e70938
Rosya01 <72784408+Rosya01@users.noreply.github.com>
2020-12-09 21:35:07 +0300

    Add files via upload

---
+++ Sokr.drob.py
@@ -0,0 +1,15 @@
+def ReduceFraction(n, m):
+    p1 = max(n, m)
+    p2 = min(n, m)
+    if p1 == p2 and p1 * p2 != 0:
+        return 1, 1
+    else:
+        p = p1 % p2
+        while p > 0:
+            p1 = p2
+            p2 = p
+            p = p1 % p2
+            return n // p2, m // p2
+n = int(input("n = "))
+m = int(input("m = "))
+print(*ReduceFraction(n, m))
